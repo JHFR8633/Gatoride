3178c91fcd6aa3724c3a3f19498ebec0fabd564b
Joe Liu <joeliu0999@gmail.com>
2024-04-19 12:53:56 -0400

    t

--- api/app/models.py
+++ api/app/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -6,36 +7,96 @@ db = SQLAlchemy()
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     make = db.Column(db.String(80))
-    model = db.Column(db.String(80)) 
+    model = db.Column(db.String(80))
+    reserve_date = db.Column(db.Text)
 
     def __repr__(self):
         return '<Car %r %r>' % (self.make, self.model)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80))
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
-    #role = db.Column(db.String(80)) # either "admin", "employee", or "client"
-                     
+    # role = db.Column(db.String(80)) # either "admin", "employee", or "client"
+
     def __repr__(self):
         return '<User %r>' % self.username
-    
+
+
 def add_data(my_dic):
-    new_user = User(email=my_dic["email"], username=my_dic["username"], password=my_dic["password"])
+    new_user = User(
+        email=my_dic["email"], username=my_dic["username"], password=my_dic["password"])
     db.session.add(new_user)
     db.session.commit()
 
+
+def update_reservation(my_Json):
+    ID = my_Json["id"]
+    start = my_Json["start_date"]
+    end = my_Json["end_date"]
+    start_ob = datetime.strptime(start, "%Y-%m-%d")
+    end_ob = datetime.strptime(end, "%Y-%m-%d")
+
+    start_epoch = int(start_ob.timestamp())
+    end_epoch = int(end_ob.timestamp())  # standard time since epoch
+
+    # if(the date range matches car) return cars
+    available_cars = Car.query.all()
+
+    update_car = Car.query.filter_by(id=ID)  # column_name = input
+
+    existing_dates = update_car.reserve_date or ''  # Get existing dates or initialize
+
+    new_dates_str = existing_dates + ',' + str(start_epoch)
+    new_dates_str = existing_dates + ',' + str(end_epoch)
+
+    update_car.reserve_date = new_dates_str
+    db.session.commit()
+
+
+def check_availability(my_Json):
+    start_date = my_Json["start_date"]
+    end_date = my_Json["end_date"]
+
+    start_ob = datetime.strptime(start_date, "%m/%d/%Y")
+    end_ob = datetime.strptime(end_date, "%m%d%Y")
+
+    start_epoch = int(start_ob.timestamp())
+    end_epoch = int(end_ob.timestamp())  # standard time since epoch
+
+    available_cars = []
+    for cars in Car.query.all():
+        existing_dates = cars.reserve_dates_text
+        existing_dates_list = existing_dates.split(',')
+
+        for i in range(0, len(existing_dates_list), 2):
+            if start_epoch > existing_dates_list[i] and start_epoch < existing_dates_list[i+1]:
+                pass
+            elif start_epoch < existing_dates_list[i] and end_epoch > existing_dates_list[i]:
+                pass
+            if i == len(existing_dates_list):
+                available_cars.append(cars)
+
+    return available_cars
+
+
 # return password and username of a user
+
+
 def gethashed_password_username(user_name):
-    user = User.query.filter_by(username=user_name).first() #.first() return the first occurance of the data, can remove after implementing dupe check
+    # .first() return the first occurance of the data, can remove after implementing dupe check
+    user = User.query.filter_by(username=user_name).first()
     if user is None:
         return ["invalid user name"]
-    return [user.username,user.password]
+    return [user.username, user.password]
+
 
 def get_hashed_password(username_):
     user = User.query.filter_by(username=username_).first()
     return user
+
 
 def create_dummy_cars():
     # List of dummy car data
@@ -46,12 +107,12 @@ def create_dummy_cars():
         {'make': 'Tesla', 'model': 'Model S'},
         {'make': 'Chevrolet', 'model': 'Impala'}
     ]
-    
+
     # Create and add dummy cars to the database
     for car_data in dummy_data:
         car = Car(make=car_data['make'], model=car_data['model'])
         db.session.add(car)
-    
+
     # Commit the transaction
     db.session.commit()
 
--- api/app/routes.py
+++ api/app/routes.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from .models import User, Car
-from .models import add_data, get_hashed_password
+from .models import add_data, get_hashed_password, check_availability, update_reservation
 from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
@@ -11,7 +11,6 @@ def configure_routes(app):
     def run():
         emails = [user.email for user in User.query.all()]
         return jsonify({"members": emails})
-
 
     @app.route('/addUser', methods=['POST'])
     def add_user():
@@ -23,80 +22,83 @@ def configure_routes(app):
                 new_user['email'] = data["email"]
                 new_user['username'] = data["username"]
                 new_user['password'] = generate_password_hash(data["password"])
-                new_user['role'] = "client" # creates a client account by default
+                # creates a client account by default
+                new_user['role'] = "client"
 
-                email_exists = User.query.filter_by(email=new_user["email"]).first() is not None
-                user_exists = User.query.filter_by(username=new_user["username"]).first() is not None
+                email_exists = User.query.filter_by(
+                    email=new_user["email"]).first() is not None
+                user_exists = User.query.filter_by(
+                    username=new_user["username"]).first() is not None
 
                 if (email_exists):
-                    return jsonify({"valid" : False, "error": "This email is already registered"}), 400
-                
+                    return jsonify({"valid": False, "error": "This email is already registered"}), 400
+
                 elif (user_exists):
-                    return jsonify({"valid" : False, "error": "This username is already taken"}), 400
-                    
+                    return jsonify({"valid": False, "error": "This username is already taken"}), 400
+
                 else:
                     add_data(new_user)
-                    return jsonify({"valid" : True, "error": "", "data" : data}), 200
-                
-            except:
-                return jsonify({"valid" : False, "error": "Some data is missing", "data": data}), 400
-            
-        
-        return jsonify({"valid" : False, "error": "Request must contain JSON data"}), 400
+                    return jsonify({"valid": True, "error": "", "data": data}), 200
 
-    
-    #input json should be name, username and password
-    @app.route('/login',methods=['POST'])
+            except:
+                return jsonify({"valid": False, "error": "Some data is missing", "data": data}), 400
+
+        return jsonify({"valid": False, "error": "Request must contain JSON data"}), 400
+
+    # input json should be name, username and password
+    @app.route('/login', methods=['POST'])
     def login():
 
-        
         username = request.json.get('username', None)
         password = request.json.get('password', None)
         user = User.query.filter_by(username=username).first()
 
         if user and check_password_hash(user.password, password):
             access_token = create_access_token(identity=username)
-            return jsonify({"data": {"token" : access_token}}), 200
-        
+            return jsonify({"data": {"token": access_token}}), 200
+
         return jsonify({"error": "Invalid login credentials"}), 401
-    
+
     @app.route('/getcars', methods=['GET'])
     def get_cars():
         cars = Car.query.all()
-        
-        car_data = [{'id': car.id, 'make': car.make, 'model': car.model} for car in cars]
+
+        car_data = [{'id': car.id, 'make': car.make, 'model': car.model}
+                    for car in cars]
 
         response = jsonify(car_data)
         # Allow requests from all origins
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
-    
     # recieve date and return avaliable car object
-    #incoming format {"start_date":"2010","end_date":"2020"}
-    @app.route('/rent/date',methods=['POST','GET'])
+    # incoming format {"start_date":"2010...","end_date":"2020..."}
+
+    @app.route('/rent/date', methods=['POST', 'GET'])
     def check_aviabale_cars():
-        dates=request.get_json()
-        start_data=dates["start_date"]
-        end_date=dates["end_date"]
+        dates = request.get_json()
+        available_cars = Car.query.all()
+        available_cars = check_availability(dates)
 
-        #if(the date range matches car) return cars
-        available_cars=Car.query.all()
+        return jsonify([{'id': car.id, 'make': car.make, 'model': car.model} for car in available_cars])
 
-        return jsonify([{'id': car.id, 'make':car.make, 'model': car.model} for car in available_cars])
-    
     @app.route('/login/protected', methods=['GET'])
     @jwt_required()
     def protected():
         current_user = get_jwt_identity()
-        if ( current_user is None ):
-            return jsonify({ "valid" : False, "error" : "Invalid Token", "data" : None }), 401
-        
+        if (current_user is None):
+            return jsonify({"valid": False, "error": "Invalid Token", "data": None}), 401
+
         user_match = User.query.filter_by(username=current_user).first()
 
-        if( user_match is None):
-            return jsonify({ "valid" : False, "error" : "Invalid Token", "data" : None }), 401
-        
-        else:
-            return jsonify({ "valid" : True, "error" : "", "data" : { "username" : user_match.username, "email" : user_match.email } }), 200
+        if (user_match is None):
+            return jsonify({"valid": False, "error": "Invalid Token", "data": None}), 401
 
+        else:
+            return jsonify({"valid": True, "error": "", "data": {"username": user_match.username, "email": user_match.email}}), 200
+
+    # incoming format start 0004-03-24 end ID
+    @app.route('/reserve', methods=['POST'])
+    def reserve_car():
+        reserve = request.get_json()
+        update_reservation(reserve)
